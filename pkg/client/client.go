2dd7893998ac08b23ef100ccaf855212bdd56d7b
Funky <mrfunky001@gmail.com>
2019-10-21 20:30:22 +0800

    change grpc func

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	pb "github.com/Mr-Linus/Pump2/pkg/pump2"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 	"time"
 )
@@ -12,7 +13,7 @@ const (
 	address     = "localhost:10088"
 )
 
-func run() {
+func RunTestWithoutTLS() {
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
@@ -37,5 +38,36 @@ func run() {
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-	log.Printf("Greeting: %s", r.GetImageName())
+	log.Printf("Recv: %s", r.GetImageName())
+}
+
+func RunTestWithTLS(tlsKeyfile string, domainName string) {
+	creds, err := credentials.NewClientTLSFromFile(tlsKeyfile, domainName)
+	if err != nil {
+		log.Fatalf("failed to load credentials: %v", err)
+	}
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(creds))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewPump2Client(conn)
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.BuildImages(ctx,
+		&pb.BuildInfo{
+			Name:"test",
+			Gpu:true,
+			Tf:true,
+			Torch:false,
+			TfVersion:"123",
+			TorchVersion:"23",
+			Dependence:"DEP",
+			UseToTest:false,
+		})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Recv: %s", r.GetImageName())
 }
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Mr-Linus/Pump2/pkg/pump2"
 	pu "github.com/Mr-Linus/Pump2/pkg/pump2"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 	"net"
 )
@@ -18,14 +19,30 @@ func (s *P2Server) BuildImages(ctx context.Context, in *pump2.BuildInfo) (*pump2
 	return &pump2.BuildResult{ BuildStats: false, ImageName:"Test"}, nil
 }
 
-func Start (IP string, Port string) {
-	lis, err := net.Listen("tcp",IP+":"+Port)
+func StartWithoutTLS (IP string, Port int) {
+	listen, err := net.Listen("tcp",IP+":"+string(Port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	pu.RegisterPump2Server(s, &P2Server{})
-	if err := s.Serve(lis); err != nil{
+	if err := s.Serve(listen); err != nil{
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func StartWithTLS (IP string, Port int, tlsCrtFile string, tlsKeyfile string) {
+	listen, err := net.Listen("tcp", IP+":"+string(Port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	creds, err := credentials.NewClientTLSFromFile(tlsCrtFile, tlsKeyfile)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer(grpc.Creds(creds))
+	pu.RegisterPump2Server(s, &P2Server{})
+	if err := s.Serve(listen); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
